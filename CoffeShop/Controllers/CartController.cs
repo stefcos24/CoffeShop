accfaf56eb67730c33f799659b6cf1298e52e64a
Stefan Cosic <stefan22bl@gmail.com>
2021-09-28 22:26:39 +0200

    Added cart

--- CoffeShop/Controllers/CartController.cs
+++ CoffeShop/Controllers/CartController.cs
@@ -1,17 +1,24 @@
 ﻿using CoffeShop.Data;
 using CoffeShop.Models;
+using CoffeShop.Models.ViewModels;
 using CoffeShop.Uttillity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace CoffeShop.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
 
         public CartController(ApplicationDbContext db)
         {
@@ -32,6 +39,58 @@ namespace CoffeShop.Controllers
             IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
 
             return View(prodList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+           return RedirectToAction(nameof(Summary));
+        }
+
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            //var userId = User.FindFirstValue(ClaimTypes.Name);
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                //session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = prodList
+            };
+
+            return View(ProductUserVM);
+        }
+
+
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                //session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(u => u.ProductId == id));
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
