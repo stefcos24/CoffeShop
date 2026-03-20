89288a0e7117285ed027798db86a6a33e1cdc60d
stefcos24 <stefan22bl@gmail.com>
2026-03-20 08:40:02 +0100

    Update from NET5 to NET10 and add env example

--- CoffeShop/Startup.cs
+++ CoffeShop/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
@@ -45,7 +46,7 @@ namespace CoffeShop
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -55,9 +56,36 @@ namespace CoffeShop
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // HSTS/HTTPS redirection are optionally controlled below via UseHttpsRedirection.
+            }
+
+            // Allow Docker (and other environments) to disable HTTPS redirection.
+            var useHttpsRedirection = Configuration.GetValue<bool?>("UseHttpsRedirection") ?? true;
+            if (!env.IsDevelopment() && useHttpsRedirection)
+            {
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            if (useHttpsRedirection)
+            {
+                app.UseHttpsRedirection();
+            }
+
+            // Optionally run EF Core migrations on container startup.
+            var runMigrationsOnStartup = Configuration.GetValue<bool?>("RunMigrationsOnStartup") ?? false;
+            if (runMigrationsOnStartup)
+            {
+                try
+                {
+                    using var scope = app.ApplicationServices.CreateScope();
+                    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Failed to apply database migrations on startup.");
+                    throw;
+                }
+            }
             app.UseStaticFiles();
 
             app.UseRouting();
